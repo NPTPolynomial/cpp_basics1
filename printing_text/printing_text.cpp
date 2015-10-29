dd68be156aa754264de06e5a0e44d4cbff743c4c
NPTPolynomial <chengcao8@gmail.com>
2015-10-28 19:51:45 -0700

    added example of multiple \n new lines

--- printing_text/printing_text.cpp
+++ printing_text/printing_text.cpp
@@ -6,7 +6,8 @@ int main(){
     cout <<"This is text"<<" That you ca priting out "<<"And this is more"<<endl;
 
     cout <<"This is some more text \n";
-
+    
+    cout <<"This is line #1 \n Line #2 \n Line #3"<<endl;
     return 0;
 
 }
