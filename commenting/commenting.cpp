cf7d6051735bd1a5fedfcc476b83068f3b065db3
NPTPolynomial <chengcao8@gmail.com>
2015-10-28 19:57:32 -0700

    added multiline commenting

--- commenting/commenting.cpp
+++ commenting/commenting.cpp
@@ -12,6 +12,16 @@ int main(){
     
     //cout << "This is text"<<endl
 
+    cout << "This is another line of example text"<<endl;
+
+
+    /*
+    commenting mutiple lines
+    this way you can write a lot of things inside here
+    and no need to keep adding slashes to each line
+    */
+
+
     return 0;
 
 }
