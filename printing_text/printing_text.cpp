0817e7e9160f3b897fbba662d90cee976a754b5f
NPTPolynomial <chengcao8@gmail.com>
2015-10-28 19:35:15 -0700

    Added printing text example

---
+++ printing_text/printing_text.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout <<"This is text"<<" That you ca priting out "<<"And this is more"<<endl;
+
+    cout <<"This is some more text"<<endl;
+
+    return 0;
+
+}
