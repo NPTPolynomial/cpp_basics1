d014d935979a454dc92cbdcb359a45a2c354db52
NPTPolynomial <chengcao8@gmail.com>
2015-10-28 19:55:32 -0700

    added commenting example

---
+++ commenting/commenting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "This is text"<<endl;
+
+    // This is a comment
+
+    // The bellow is a line of code that has been commeted.
+    
+    //cout << "This is text"<<endl
+
+    return 0;
+
+}
