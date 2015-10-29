cf2d4b374421c4cf18cc58f043fdc850ac3d4239
NPTPolynomial <chengcao8@gmail.com>
2015-10-28 20:02:57 -0700

    added variabled example. int

---
+++ variables/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "This is the variable example"<<endl;
+    cout << "This is an integer (int)"<<endl;
+
+    int myInt = 5;
+
+    cout << "Printing myInt: " << myInt << endl;
+
+
+}
